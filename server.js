2baa6a58a738ad65a2c90115849cbfe775fc5b9d
66kay <66kay@users.noreply.github.com>
2026-08-26 19:33:53 -0400

    Actualizacion: optimice la fluidez de renderizado de la pagina y actualice la URL del backend de Render a escombros-amaury-rojas

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5173',
   'http://localhost:3000',
   'http://127.0.0.1:3000',
-  'https://escombros-amaury-rojas-web-pag.onrender.com',
+  'https://escombros-amaury-rojas.onrender.com',
   'https://escombrosamauryrojas.cl',
   'https://www.escombrosamauryrojas.cl'
 ];
